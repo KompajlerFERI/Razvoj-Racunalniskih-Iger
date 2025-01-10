fd01d3fc3c35686387c67a983d4aebc0ea74abfa
aljosaferi <aljosa.golob1@student.um.si>
2025-01-10 13:53:32 +0100

    [DD-310] Added player and player images

--- core/src/main/java/si/um/feri/kompajler/assets/AssetPaths.java
+++ core/src/main/java/si/um/feri/kompajler/assets/AssetPaths.java
@@ -1,6 +1,7 @@
 package si.um.feri.kompajler.assets;
 
 public class AssetPaths {
+    public static final String GAMEPLAY = "packed_assets/images.atlas";
 
     private AssetPaths() {
     }
---
+++ core/src/main/java/si/um/feri/kompajler/gameplay/Player.java
@@ -0,0 +1,30 @@
+package si.um.feri.kompajler.gameplay;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import si.um.feri.kompajler.assets.RegionNames;
+
+public class Player {
+    private int hitpoints;
+    private TextureRegion tankBottom;
+    private TextureRegion tankTop;
+
+    public Player(TextureAtlas atlas) {
+        this.hitpoints = 1;
+        this.tankBottom = atlas.findRegion(RegionNames.TANK_BOTTOM_GREEN);
+        this.tankTop = atlas.findRegion(RegionNames.TANK_TOP_GREEN);
+    }
+
+    public int getHitpoints() {
+        return hitpoints;
+    }
+
+    public TextureRegion getTankBottom() {
+        return tankBottom;
+    }
+
+    public TextureRegion getTankTop() {
+        return tankTop;
+    }
+}
